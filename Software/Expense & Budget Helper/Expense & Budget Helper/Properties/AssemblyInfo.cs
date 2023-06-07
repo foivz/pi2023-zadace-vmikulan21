c182aec5f240bc7526517f32454b2c12d3e58b88
vmikulan21 <vmikulan21@student.foi.hr>
2023-06-07 17:52:22 +0200

    Promijenjena ikona aplikacije i napravljena instalacija aplikacije s potrebnim datotekama.

--- Software/Expense & Budget Helper/Expense & Budget Helper/Properties/AssemblyInfo.cs
+++ Software/Expense & Budget Helper/Expense & Budget Helper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Expense & Budget Helper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Expense and Budget Helper")]
+[assembly: AssemblyDescription("Expense and budget helper helps with tracking expenses")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Expense & Budget Helper")]
+[assembly: AssemblyCompany("FOI")]
+[assembly: AssemblyProduct("Expense and Budget Helper")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
